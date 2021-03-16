663588027da4cc3b270bfe7ba4d5bc59c4ea6805
Samar1105 <samar.gagneja05@gmail.com>
2021-03-16 11:27:30 +0530

    Modify

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const app = express ();
 // set our port
 const port = 3000 ;
 app.get ( '/' , ( req , res ) => res.send ( 'Welcome to Mean stack : Summer Gagneja 1805564' ));// startup our app at http://localhost:3000
+app.get('/contactus', (req, res) => res.send('Welcome to Contact Us Page'))
+app.get('/about', (req, res) => res.send('Welcome to About Page'))
+
+app.get('/product', (req, res) => res.send('Welcome to Products Page'))
+app.get('/product/:id', (req, res) => {
+    res.send('Welcome to Product Detailed Page');
+})
 app.listen ( port , () =>console.log ( `Example app listening on port ${port}!` ));
